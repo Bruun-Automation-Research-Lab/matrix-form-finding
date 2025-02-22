451b572bfbf5897380e290abd636a7f1a99af2c4
ebruun <edvard.bruun@ce.gatech.edu>
2025-02-22 11:09:42 -0500

    reorganized import funcs as modules

--- helper_matrix.py
+++ helper_matrix.py
@@ -1,38 +1,6 @@
 import numpy as np
 
 
-def nodes_delta(p_x, p_y, p_z, K, D, D_f, x, y, z, x_f, y_f, z_f):
-    # Compute the right-hand side vector (p - D * x - D_f * x_f)
-    rhs_x = p_x - D @ x - D_f @ x_f
-    rhs_y = p_y - D @ y - D_f @ y_f
-    rhs_z = p_z - D @ z - D_f @ z_f
-
-    # Compute the inverse of D
-    K_inv = np.linalg.inv(K)
-
-    # Solve for the displacements (delta)
-    delta_x = K_inv @ rhs_x
-    delta_y = K_inv @ rhs_y
-    delta_z = K_inv @ rhs_z
-
-    return delta_x, delta_y, delta_z
-
-
-def nodes_update(nodes, d_x, d_y, d_z, n_f):
-    # Flatten the free_node_mask to match the shape of n_f
-    free_node_mask = n_f.flatten() == 0
-
-    # Create a copy of the original nodes array to avoid in-place modification
-    updated_nodes = np.copy(nodes)
-
-    # Apply displacement to free nodes
-    updated_nodes[free_node_mask, 0] += d_x.flatten()  # Update x-coordinates
-    updated_nodes[free_node_mask, 1] += d_y.flatten()  # Update y-coordinates
-    updated_nodes[free_node_mask, 2] += d_z.flatten()  # Update z-coordinates
-
-    return updated_nodes
-
-
 def generate_struct_arrays(
     nodes, elements, elements_preload, nodes_load, nodes_fixed
 ):
@@ -272,28 +240,3 @@ def create_force_matrix(L, L_0, E, A, F_0):
     # F = np.diag(forces)
 
     return np.diag(forces)
-
-
-def compute_kinetic_energy(K, v_x, v_y, v_z, h):
-    """
-    Compute the kinetic energy given a diagonal mass matrix K_mod
-    and velocity components v_x, v_y, v_z.
-
-    Parameters:
-    K_mod : (N, N) numpy.ndarray - Diagonal mass matrix (N x N)
-    v_x, v_y, v_z : (N,) numpy.ndarray - Velocity components
-
-    Returns:
-    KE : float - Total kinetic energy
-    """
-    # Extract the diagonal elements (masses)
-    masses = np.diag(K) * h**2 / 2
-
-    # Compute kinetic energy for each direction
-    KE_x = 0.5 * np.dot(v_x * masses, v_x)
-    KE_y = 0.5 * np.dot(v_y * masses, v_y)
-    KE_z = 0.5 * np.dot(v_z * masses, v_z)
-
-    # Total kinetic energy
-    KE = np.sum(KE_x + KE_y + KE_z)
-    return KE
--- solver.py
+++ solver.py
@@ -1,93 +1,40 @@
 import numpy as np
 
+import helper_log as hl
+import helper_solver as hs
+import helper_matrix as hm
+import helper_plot as hp
+
 from structures.struct_4 import generate_struct
-
-from helper_matrix import (
-    nodes_delta,
-    nodes_update,
-    generate_struct_arrays,
-    create_connectivity_matrix,
-    create_length_matrix,
-    create_node_force_vectors,
-    create_elastic_stiffness_matrix,
-    compute_kinetic_energy,
-    create_force_matrix,
-    partition_connectivity_matrix,
-    partition_nodes_coordinates,
-)
-from helper_plot import (
-    plot_network_views,
-    plot_network3D,
-    plot_animation,
-    plot_kinetic_energy,
-    plot_quadratic_interp,
-)
-from helper_log import (
-    setup_logging,
-    debug_initial_struct,
-    debug_iteration,
-    debug_force_and_density,
-    debug_stiffness,
-    debug_stiffness_FD,
-    debug_deltas,
-    debug_new_nodes,
-    debug_velocity_kinetic_energy,
-    debug_energy_peak,
-    debug_table,
-    debug_error,
-    debug_final,
-)
-
-
-def quadratic_interp(y_points, num_points=100):
-    x_points = np.array([0.0, 0.5, 1.0])
-
-    # Fit a quadratic polynomial
-    coeffs = np.polyfit(x_points, y_points, 2)
-
-    # Extract the coefficients a, b, and c
-    a, b, c = coeffs
-
-    # Find the x-value where the maximum occurs (vertex of the parabola)
-    x_max = -b / (2 * a)
-
-    # Find the maximum y-value
-    y_max = np.polyval(coeffs, x_max)
-
-    # Generate smooth x values for plotting
-    x_interp = np.linspace(min(x_points), max(x_points), num_points)
-    y_interp = np.polyval(coeffs, x_interp)
-
-    return x_max, y_max, x_interp, y_interp
 
 
 # Main computation
 def main(debug=False, solver="FD_fixed"):
-    setup_logging(debug)
+    hl.setup_logging(debug)
 
     # nodes, elements, external_loads, fixed_nodes = generate_struct(5)
     nodes, elements, elements_preload, nodes_load, nodes_fixed = (
         generate_struct()
     )
 
-    n, e, e_l, n_l, n_f = generate_struct_arrays(
+    n, e, e_l, n_l, n_f = hm.generate_struct_arrays(
         nodes, elements, elements_preload, nodes_load, nodes_fixed
     )
 
     # Plot network
-    plot_network_views(n, e, n_l, n_f)
+    hp.plot_network_views(n, e, n_l, n_f)
 
     # Calculate initial element lengths
-    l_vec, L = create_length_matrix(n, e)
+    l_vec, L = hm.create_length_matrix(n, e)
     L_total = np.sum(l_vec**2)
 
     # Generate connectivity matrix
-    connectivity_matrix = create_connectivity_matrix(n, e)
+    connectivity_matrix = hm.create_connectivity_matrix(n, e)
 
-    C, C_f = partition_connectivity_matrix(connectivity_matrix, n_f)
+    C, C_f = hm.partition_connectivity_matrix(connectivity_matrix, n_f)
 
     # Compute forces on free nodes
-    p_x, p_y, p_z = create_node_force_vectors(n_l, n_f)
+    p_x, p_y, p_z = hm.create_node_force_vectors(n_l, n_f)
 
     # Create a list to hold node positions at each iteration for animation
     node_positions = []
@@ -115,22 +62,22 @@ def main(debug=False, solver="FD_fixed"):
     KE_prev = 0.0  # KE at t-1
     KE_history = []
 
-    debug_initial_struct(
+    hl.debug_initial_struct(
         n, e, e_l, n_l, n_f, connectivity_matrix, C, C_f, p_x, p_y, p_z
     )
 
     # Initialize iteration
     for iteration in range(MAX_ITER):
-        debug_iteration(iteration, solver)
+        hl.debug_iteration(iteration, solver)
 
-        x, y, z, x_f, y_f, z_f = partition_nodes_coordinates(n, n_f)
-        l_vec, L = create_length_matrix(n, e)
+        x, y, z, x_f, y_f, z_f = hm.partition_nodes_coordinates(n, n_f)
+        l_vec, L = hm.create_length_matrix(n, e)
 
         if solver == "FD_fixed":
             F = L
             Q = FD_factor * F @ np.linalg.inv(L)
             # Q = np.diag(Q @ e_l.flatten()) # A way to apply pre-load
-            debug_force_and_density(F, Q)
+            hl.debug_force_and_density(F, Q)
 
             # Stiffness matrix
             K = C.T @ Q @ C
@@ -139,19 +86,19 @@ def main(debug=False, solver="FD_fixed"):
             D = C.T @ Q @ C
             D_f = C.T @ Q @ C_f
 
-            debug_stiffness_FD(K, D, D_f)
+            hl.debug_stiffness_FD(K, D, D_f)
 
             # Compute new positions
-            d_x, d_y, d_z = nodes_delta(
+            d_x, d_y, d_z = hs.nodes_delta(
                 p_x, p_y, p_z, K, D, D_f, x, y, z, x_f, y_f, z_f
             )
 
-            debug_deltas(d_x, d_y, d_z)
+            hl.debug_deltas(d_x, d_y, d_z)
 
         elif solver == "FD_iter":
             F = FD_factor * np.eye(len(e))
             Q = F @ np.linalg.inv(L)
-            debug_force_and_density(F, Q)
+            hl.debug_force_and_density(F, Q)
 
             # Stiffness matrix (free nodes)
             K = C.T @ Q @ C
@@ -160,23 +107,23 @@ def main(debug=False, solver="FD_fixed"):
             D = C.T @ Q @ C
             D_f = C.T @ Q @ C_f
 
-            debug_stiffness_FD(K, D, D_f)
+            hl.debug_stiffness_FD(K, D, D_f)
 
             # Compute change in x,y,z
-            d_x, d_y, d_z = nodes_delta(
+            d_x, d_y, d_z = hs.nodes_delta(
                 p_x, p_y, p_z, K, D, D_f, x, y, z, x_f, y_f, z_f
             )
 
-            debug_deltas(d_x, d_y, d_z)
+            hl.debug_deltas(d_x, d_y, d_z)
 
         elif solver == "DR":
-            F = create_force_matrix(L, L_0, E, A, F_0)
+            F = hm.create_force_matrix(L, L_0, E, A, F_0)
             Q = F @ np.linalg.inv(L)
-            debug_force_and_density(F, Q)
+            hl.debug_force_and_density(F, Q)
 
             # Element-level stiffness matrices
             K_g = Q
-            K_e = create_elastic_stiffness_matrix(E, A, L_0)
+            K_e = hm.create_elastic_stiffness_matrix(E, A, L_0)
             K_total = K_g + K_e
 
             # Free Nodes stiffness matrices
@@ -189,13 +136,13 @@ def main(debug=False, solver="FD_fixed"):
             # K_mod = K * delta
             K_mod = K
 
-            debug_stiffness(K_g, K_e, K, K_mod)
+            hl.debug_stiffness(K_g, K_e, K, K_mod)
 
             D = C.T @ Q @ C
             D_f = C.T @ Q @ C_f
 
             # Compute new positions
-            x, y, z = nodes_delta(
+            x, y, z = hs.nodes_delta(
                 p_x, p_y, p_z, K_mod, D, D_f, x, y, z, x_f, y_f, z_f
             )
 
@@ -217,11 +164,11 @@ def main(debug=False, solver="FD_fixed"):
             d_y = v_y * h
             d_z = v_z * h
 
-            KE = compute_kinetic_energy(K_mod, v_x, v_y, v_z, h)
+            KE = hs.compute_kinetic_energy(K_mod, v_x, v_y, v_z, h)
 
-            debug_velocity_kinetic_energy(v_x, v_y, v_z, KE)
-            debug_deltas(d_x, d_y, d_z)
-            debug_table([KE_prev2, KE_prev, KE])
+            hl.debug_velocity_kinetic_energy(v_x, v_y, v_z, KE)
+            hl.debug_deltas(d_x, d_y, d_z)
+            hl.debug_table([KE_prev2, KE_prev, KE])
 
             check_KE = True
 
@@ -231,10 +178,10 @@ def main(debug=False, solver="FD_fixed"):
                 q1 = (KE_prev - KE) / ((KE_prev - KE) - (KE_prev2 - KE_prev))
 
                 # # this is same interp, different interval than the paper
-                # q2, KE_q, x_interp, y_interp = quadratic_interp(
+                # q2, KE_q, x_interp, y_interp = hs.quadratic_interp(
                 #     [KE_prev2, KE_prev, KE]
                 # )
-                # plot_quadratic_interp(
+                # hp.plot_quadratic_interp(
                 #     [0, 0.5, 1.0],
                 #     [KE_prev2, KE_prev, KE],
                 #     x_interp,
@@ -246,17 +193,17 @@ def main(debug=False, solver="FD_fixed"):
                 # )
 
                 q = q1
-                debug_energy_peak(q1)
+                hl.debug_energy_peak(q1)
 
                 d_x -= h * (1 + q) * gamma * v_x + gamma * (q) * x
                 d_y -= h * (1 + q) * gamma * v_y + gamma * (q) * y
                 d_z -= h * (1 + q) * gamma * v_z + gamma * (q) * z
-                debug_deltas(d_x, d_y, d_z)
+                hl.debug_deltas(d_x, d_y, d_z)
 
                 # Reset velocities to 0 (kinetic damping)
                 first = True
 
-                # _ = debug_table(q1, KE_q, [KE_prev2, KE_prev, KE])
+                # _ = hl.debug_table(q1, KE_q, [KE_prev2, KE_prev, KE])
                 # KE = np.copy(KE_q)
 
             KE_prev2 = np.copy(KE_prev)
@@ -265,11 +212,11 @@ def main(debug=False, solver="FD_fixed"):
             KE_history.append(KE)
 
         # Update nodes
-        n_new = nodes_update(n, d_x, d_y, d_z, n_f)
+        n_new = hs.nodes_update(n, d_x, d_y, d_z, n_f)
         node_positions.append(n_new)
 
         # Check for convergence
-        l_vec, L_new = create_length_matrix(n_new, e)
+        l_vec, L_new = hm.create_length_matrix(n_new, e)
         L_total_new = np.sum(l_vec**2)
         error = np.abs(L_total_new - L_total)
 
@@ -284,26 +231,26 @@ def main(debug=False, solver="FD_fixed"):
         L = L_new
         L_total = L_total_new
 
-        debug_new_nodes(n_new)
-        debug_error(L_total, error)
+        hl.debug_new_nodes(n_new)
+        hl.debug_error(L_total, error)
 
         if error < TOL:
             print("Convergence achieved!")
             break
 
-        # plot_network3D(n, e, n_l, n_f)
+        # hp.plot_network3D(n, e, n_l, n_f)
 
     else:
         print("Max iterations reached without convergence.")
 
     # Final Structure
-    debug_final(n_new, L_new, F, Q)
+    hl.debug_final(n_new, L_new, F, Q)
 
-    plot_kinetic_energy(KE_history, solver)
+    hp.plot_kinetic_energy(KE_history, solver)
 
-    plot_animation(node_positions, e, n_f, t=1)
+    hp.plot_animation(node_positions, e, n_f, t=1)
 
-    plot_network_views(n, e, n_l, n_f)
+    hp.plot_network_views(n, e, n_l, n_f)
 
 
 if __name__ == "__main__":
